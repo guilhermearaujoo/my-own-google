8929df7ac106e101badc3ca76487a3eb423637c8
guilhermearaujoo <ghma.em1@gmail.com>
2022-10-26 00:54:48 -0300

    Feito mobile

--- page/script.js
+++ page/script.js
@@ -1,7 +1,19 @@
-const showSuspendMenu = () => {
-  const menu = document.getElementsByClassName('itens-header')[0];
+const menu = document.getElementsByClassName('itens-header')[0];
+const main = document.getElementsByTagName('main')[0];
+const footer = document.getElementsByClassName('footer')[0];
 
-  (menu.style.display === 'none') ? menu.style.display = 'grid' : menu.style.display = 'none';
+const showSuspendMenu = () => {
+  if (menu.style.display === 'none' || menu.style.display === '') {
+    menu.style.display = 'grid';
+    if(screen.width < 1100) {
+      main.style.filter = 'brightness(20%)';
+      footer.style.filter = 'brightness(20%)';
+      }; 
+  } else {
+    menu.style.display = 'none';
+    main.style.filter = 'none';
+    footer.style.filter = 'none';
+  }
 }
 
 const button = document.getElementById('mini-header');
